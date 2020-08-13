97acb64438ff5cdd16aa9e0051f70502f3b74372
c00kamunga <tsothalanti2@yahoo.com>
2020-08-13 16:26:08 -0700

    created an endpoint for getting a user, getting a user by ID, and deleting a user

--- users/userRouter.js
+++ users/userRouter.js
@@ -1,11 +1,12 @@
 const express = require('express');
+const userDb = require('./userDb');
 
 const router = express.Router();
 
 router.post('/', (req, res) => {
   if(req.body.name) {
     const newUser = db.insert({name: req.body.name})
-    const users = db.get()
+    const users = userDB.get()
     if (users) {
       res.json(users)
       res.status(201)
@@ -25,12 +26,37 @@ router.post('/:id/posts', (req, res) => {
   // do your magic!
 });
 
-router.get('/', (req, res) => {
-  // do your magic!
+router.get('/', async (req, res) => {
+  try {
+    const users = await userDb.get()
+    res.status(200).json(users)
+  } catch (error) {
+    res
+    .status(500)
+    .json({ error: "The user information could not be retrieved" })
+  }
 });
 
 router.get('/:id', (req, res) => {
   // do your magic!
+  const id = req.params.id
+  const user = userDb.getById(id)
+
+  if(user) {
+    try {
+      res
+      .json(user)
+    } 
+    catch(error) {
+      res
+      .status(500)
+      .json ({ errorMessage: "User information could not be found" })
+    }
+  } else {
+    res
+    .status(404)
+    .json({ message: "user with specified ID does not exist" })
+  }
 });
 
 router.get('/:id/posts', (req, res) => {
@@ -38,7 +64,21 @@ router.get('/:id/posts', (req, res) => {
 });
 
 router.delete('/:id', (req, res) => {
-  // do your magic!
+  const id = req.params.id
+  const userToDelete = userDb.remove(id)
+
+  if(!userToDelete) {
+res.status(500).json({ errorMessage: "This does not work!" })
+  } else if(userToDelete) {
+    db.remove(id)
+    res
+    .status(200)
+    .json(userToDelete)
+  } else {
+    res
+    .status(404)
+    .json({ errorMessage: "The user ID does not exist" })
+  }
 });
 
 router.put('/:id', (req, res) => {
