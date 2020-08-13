d0ea8bfaadb33ea96ae720ee02b84d08eda7c579
c00kamunga <tsothalanti2@yahoo.com>
2020-08-13 16:44:04 -0700

    created an endpoint for getting a post by its ID

--- posts/postRouter.js
+++ posts/postRouter.js
@@ -1,4 +1,7 @@
 const express = require('express');
+const dbConfig = require('../data/dbConfig');
+const postDb = require('./postDb');
+const server = require('../server');
 
 const router = express.Router();
 
@@ -20,8 +23,26 @@ router.get('/', async (req, res) => {
 ///////////////////////////////////////////////////
 
 router.get('/:id', (req, res) => {
-  // do your magic!
+  postDb.getById(req.params.id)
+  .then(post => {
+    if(post.length === 0) {
+      return res
+      .status(404)
+      .json({ message: "The post with the specified ID does not exist." })
+    } else {
+      res
+      .status(200)
+      .json(post)
+    }
+  })
+  .catch(error => {
+    res
+    .status(500)
+    .json({ error: "The post information could not be retrieved." })
+  })
 });
+
+////////////////////////////////////////////////
 
 router.delete('/:id', (req, res) => {
   // do your magic!
